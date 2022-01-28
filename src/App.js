9ba4742340b5a2d03dfd359276124d469c477800
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-01-28 21:07:51 +0900

    events & prop driling

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 
 function App() {
-  const [feedback, useFeedback] = useState(FeedbackData);
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
 
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
